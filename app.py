949e2e341d229e7a80d9cd5baae8d2131f3e00dd
Faraj Siddique <fas5225@psu.edu>
2023-04-25 05:27:19 -0400

    Finished bidding

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import sqlite3 as sql
 from passlib.hash import sha256_crypt
+import secrets
 
 app = Flask(__name__)
-
+app.secret_key = secrets.token_hex(16)
 host = 'http://127.0.0.1:5000/'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('input.html')
 
 @app.route('/name', methods=['POST', 'GET'])
 def name():
@@ -16,20 +17,19 @@ def name():
     if request.method == 'POST':
         result = valid_name(request.form['username'], request.form['password'])
         email = request.form['username']
+        session['username'] = email
         if result:
             role = request.form['user-type']
+            session['role'] = role
             if role == 'bidder':
                 connection = sql.connect('database.db')
                 cursor = connection.execute('SELECT * from bidders WHERE email=?;', (email, ))
                 data = cursor.fetchone()
-                print(data)
                 if data is not None:
                     cursor2 = connection.execute('SELECT * from address WHERE address_id=?;', (data[5], ))
                     address_data = cursor2.fetchone()
-                    print(address_data)
                     cursor2 = connection.execute('SELECT * from zipcode_info WHERE zipcode=?;', (address_data[1], ))
                     zipcode_data = cursor2.fetchone()
-                    print(zipcode_data)
                     street_address = f'{address_data[2]} {address_data[3]} {zipcode_data[1]}, {zipcode_data[2]} {zipcode_data[0]}'
                     cursor.close()
                     cursor2.close()
@@ -94,7 +94,7 @@ def hash_passwords():
 def categories():
     connection = sql.connect("database.db")
     cursor = connection.cursor()
-    categories_list = cursor.execute('SELECT * FROM categories').fetchall()
+    categories_list = cursor.execute('SELECT * FROM categories ORDER BY parent_category').fetchall()
     cursor.close()
     connection.close()
     return render_template('categories.html', categories=categories_list)
@@ -113,6 +113,59 @@ def item_page(item):
     cursor = connection.execute('SELECT * from auction_listings WHERE category = ?', (item, ))
     available_items_list = cursor.fetchall()
     return render_template("item.html", item_list=available_items_list)
+
+
+@app.route('/bid/int:<item_num>', methods=['POST', 'GET'])
+def bid(item_num):
+    connection = sql.connect('database.db')
+    cursor = connection.execute('SELECT * from bids WHERE listing_id=? ORDER BY bid_price ASC', (item_num,))
+    bid_list = cursor.fetchall()
+    cursor = connection.execute('SELECT * FROM auction_listings WHERE listing_id=?', (item_num,))
+    item_info = cursor.fetchone()
+    message = 'You have not placed a bid yet'
+    max_bids = session.get('max_bids', item_info[8] - len(bid_list))
+    previous_bidder = session.get('bidder', None)
+
+    if bid_list:
+        last_price = max(bid_list, key=lambda x: x[4])[4]
+        session['previous_bid'] = last_price
+
+    if request.method == 'POST':
+        previous_bid = session.get('previous_bid', 0)
+        current_bid = int(request.form['current_bid'])
+        current_bidder = session.get('username', None)
+
+        if current_bidder == previous_bidder:
+            message = 'You have already placed a bid'
+        elif current_bid >= previous_bid + 1:
+            session['previous_bid'] = current_bid
+            session['max_bids'] = max_bids - 1
+            session['bidder'] = current_bidder
+            if session['max_bids'] == 0:
+                if item_info[7] <= current_bid:
+                    message = 'Congratulations! You have won the auction!'  ##COMPARE WITH RESERVE PRICE
+                else:
+                    message = 'Sorry! This auction was unsuccessful.'
+            else:  ##Write into DB
+                message = f'Your current bid of {current_bid} has been accepted.'
+            connection = sql.connect('database.db')
+            cursor = connection.cursor()
+            cursor.execute('SELECT MAX(bid_id) FROM bids')
+            new_bid_id = cursor.fetchone()[0] + 1
+            seller_email = item_info[0]
+            listing_id = item_info[1]
+            bidder_email = session.get('username')
+            cursor.execute('INSERT INTO bids (bid_id, seller_email, listing_id, bidder_email, bid_price) VALUES (?,?,?,?,?)',
+                            (new_bid_id, seller_email, listing_id, bidder_email, current_bid))
+            connection.commit()
+        else:
+            message = f'Your current bid of {current_bid} is not valid.'
+        max_bids = session.get('max_bids')
+        cursor = connection.execute('SELECT * from bids WHERE listing_id=? ORDER BY bid_price ASC', (item_num,))
+        bid_list = cursor.fetchall()
+        connection.close()
+    return render_template('bid.html', item_info=item_info, message=message, max_bids=max_bids, bid_list=bid_list)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
